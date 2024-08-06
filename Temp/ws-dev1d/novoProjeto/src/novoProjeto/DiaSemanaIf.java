09b7fad1f8ab377934e2c374d6e2cf5b73b8b52f
Miguel281207 <159180695+Miguel281207@users.noreply.github.com>
2024-08-06 10:48:14 -0300

    Add files via upload

---
+++ Temp/ws-dev1d/novoProjeto/src/novoProjeto/DiaSemanaIf.java
@@ -0,0 +1,44 @@
+package novoProjeto;
+
+import java.util.Scanner;
+
+public class DiaSemanaIf {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Insira um número: ");
+		int num = sc.nextInt();
+		
+		String dia;
+		
+		if (num == 1) {
+			dia = "Domingo";
+		}
+		else if (num == 2) {
+			dia = "Segunda";
+		}
+		else if (num == 3) {
+			dia = "Terça";
+		}
+		else if (num == 4) {
+			dia = "Quarta";
+		}
+		else if (num == 5) {
+			dia = "Quinta";
+		}
+		else if (num == 6) {
+			dia = "Sexta";
+		}
+		else if (num == 7) {
+			dia = "Sábado";
+		}
+		else {
+			dia = "Valor inválido";
+		}
+		System.out.println("Diga da semana: " + dia);
+		
+		sc.close();
+	}
+
+}
---
+++ Temp/ws-dev1d/programacao_backend/src/programacao_backend/Main.java
@@ -0,0 +1,20 @@
+package programacao_backend;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um valor ");
+		int v1 = sc.nextInt();
+		
+		System.out.print("Digite outro valor: ");
+		int v2 = sc.nextInt();
+		
+		System.out.println("O total da soma é: " + (v1 + v2));
+		sc.close();
+	}
+
+}
