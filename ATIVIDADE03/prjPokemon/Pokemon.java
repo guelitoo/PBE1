24e98111118e1435bca590f60dab9b3d9c094832
Miguel281207 <159180695+Miguel281207@users.noreply.github.com>
2024-08-27 12:05:56 -0300

    Update Pokemon.java

--- ATIVIDADE03/prjPokemon/Pokemon.java
+++ ATIVIDADE03/prjPokemon/Pokemon.java
@@ -18,7 +18,7 @@ public class Pokemon {
 		this.nivel = nivel;
 		this.hp = hp;
 	}
-	
+	//Metodos
 	void atacar() {
         System.out.println(nome + " usou um ataque!");
     }
@@ -27,10 +27,10 @@ public class Pokemon {
         System.out.println(nome + " evoluiu!");
     }
     void exibirinfo(){
-        System.out.println("Nome: " + this.nome);
+        System.out.println();
+	System.out.println("Nome: " + this.nome);
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Nível: " + this.nivel);
         System.out.println("HP: " + this.hp);
-        System.out.println();
     }
 }
