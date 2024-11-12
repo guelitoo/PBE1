8a767664b874d376b05b56022e60b72471dec4e8
guelitoo <159180695+guelitoo@users.noreply.github.com>
2024-11-12 13:25:10 -0300

    Add files via upload

---
+++ AT12/prjSaraiva/src/main/java/br/com/saraiva/principal/LivroService.java
@@ -0,0 +1,29 @@
+package br.com.saraiva.principal;
+
+import java.util.List;
+
+import br.com.saraiva.principal.entities.Livro;
+
+public class LivroService {
+
+	public Livro saveLivro(Livro livro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Livro> getAllLivro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Livro getLivroById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteLivro(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AT12/prjSaraiva/src/test/java/br/com/saraiva/principal/PrjSaraivaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saraiva.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSaraivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
